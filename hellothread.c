d1416d59d26b7539079d516fae7c1b8a506d2e78
grassele <91402260+grassele@users.noreply.github.com>
2023-04-09 23:12:38 -0700

    think it's working properly, but threads execute fully one at a time, in the order of joins

--- hellothread.c
+++ hellothread.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void run_func(void *arg) {
-    // print numbers 0-5, prefixed by thread name
+void *run_func(void *arg) {
+    char *name = arg;
+
+    for (int i = 0; i <= 5; i++) {
+        printf("%s: %d\n", name, i);
+    }
+
+    return NULL;
 }
 
 int main(void) {
-    // have main thread spawn 2 other threads
-    // pthread_create w thread name as fourth argument
-    // pthread_join to exit
+    printf("Launching threads\n");
+
+    pthread_t t1;
+    pthread_t t2;
+
+    pthread_create(&t1, NULL, run_func, "thread 1");
+    pthread_create(&t2, NULL, run_func, "thread 2");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Threads complete!\n");
 }
 
