e18d14ad8029e5abe9416a530b37860664529985
grassele <91402260+grassele@users.noreply.github.com>
2023-04-09 22:51:41 -0700

    first commit

---
+++ hellothread.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void run_func(void *arg) {
+    // print numbers 0-5, prefixed by thread name
+}
+
+int main(void) {
+    // have main thread spawn 2 other threads
+    // pthread_create w thread name as fourth argument
+    // pthread_join to exit
+}
+
